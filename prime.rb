8b95d8d25a5d5c23defc2a2126d1aed5b762ec3f
Changmin Lim <chang.lim92@gmail.com>
2018-05-25 09:11:01 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(int)
+  return false if int <= 1 
+  sqr = (int ** 0.5).to_i 
+  range = (2..sqr).to_a 
+  range.each do |i|
+    if int % i == 0 
+      return false 
+    end 
+  end 
+  return true
+end 
